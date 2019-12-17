1e6b919a2efe77d3c29fd02189b85f9bd4c9d474
Michael Slate <slatem@gmail.com>
2019-12-17 01:13:27 -0500

    new bytes

--- lib/peddler/client.rb
+++ lib/peddler/client.rb
@@ -132,7 +132,7 @@ module Peddler
       if content.start_with?('<?xml')
         headers['Content-Type'] = 'text/xml'
         @body = content
-      elsif content.start_with?('[37,') || content.start_with?('37,')
+      elsif content.start_with?('[37,') || content.start_with?('37,') || content.start_with?("\x00")
         headers['Content-Type'] = 'application/octet-stream'
         @body = content
       else
