c02df5d9fb4165e524016c55d47873da89c16ac7
Michael Slate <slatem@gmail.com>
2020-11-10 16:08:16 -0500

    add new marketplaces

--- lib/peddler/marketplace.rb
+++ lib/peddler/marketplace.rb
@@ -59,10 +59,13 @@ module Peddler
       ['A13V1IB3VIYZZH', 'FR', 'mws-eu.amazonservices.com'],
       ['APJ6JRA9NG5V4', 'IT', 'mws-eu.amazonservices.com'],
       ['A1F83G8C2ARO7P', 'GB', 'mws-eu.amazonservices.com'],
+      ['A1805IZSGTT6HS', 'NL', 'mws-eu.amazonservices.com'],
+      ['A2NODRKZP88ZB9', 'SE', 'mws-eu.amazonservices.com'],
       ['A33AVAJ2PDY3EV', 'TR', 'mws-eu.amazonservices.com'],
       ['A21TJRUUN4KGV', 'IN', 'mws.amazonservices.in'],
       ['A39IBJ37TRP1C6', 'AU', 'mws.amazonservices.com.au'],
-      ['A1VC38T7YXB528', 'JP', 'mws.amazonservices.jp']
+      ['A1VC38T7YXB528', 'JP', 'mws.amazonservices.jp'],
+      ['A19VAU5U5O7RUS', 'SG', 'mws-fe.amazonservices.com']
     ].map do |values|
       new(*values)
     end
